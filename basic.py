a27848799db007b8a8cd1440b3b0af1d70b56002
ezsx <scdcor@gmail.com>
2021-04-25 17:55:33 +0300

    cscxzxcxczzxcxc

---
+++ basic.py
@@ -0,0 +1,106 @@
+
+
+
+
+class Link:
+    def __init__(self, ver_a, ver_b, p):
+        self.ver_a = ver_a
+        self.ver_b = ver_b
+        self.p     = p
+
+    def __eq__(self, other):
+        b =(self.ver_a==other.ver_a and self.ver_b == other.ver_b) or \
+        (self.ver_a == other.ver_b and self.ver_b == other.ver_a)
+        return b
+
+    def __repr__(self):
+        return 'Link('+str(self.ver_a) + str(self.ver_b)+' weight= ' + str(self.p)+')'
+
+
+class Vertex:
+   def __init__(self,num):
+       self.pai = 0
+       self.num =num
+       self.list_link= list()
+
+   def add_link(self,l:Link):
+       self.list_link.append(l)
+   def color (self,pai):
+       pass
+
+   def __repr__(self):
+        return 'Vertex('+str(self.num)+')'
+
+   def __str__(self):
+       return self.__repr__()
+
+
+
+   def print(self):
+       for l in self.list_link:
+           print(l)
+
+class Graph:
+    def __init__(self):
+        self.verticals =list()
+        self.links = list()
+    def is_exist(self,l):
+        for i in self.links:
+            if l==i:
+                return True
+        return False
+
+    def get(self,num_A):
+        for v in self.verticals:
+            if v.num == num_A:
+                return v
+        v_new = Vertex(num_A)
+        self.verticals.append(v_new)
+        return v_new
+
+    def load_file(self,file):
+        f = open(file)
+
+        for line in f:
+            strs = line.split(' ')
+            v=[]
+            for s in strs:
+                if s != '':
+                    v.append(int(s))
+            ver_a = self.get(v[0])
+            ver_b = self.get(v[1])
+            L=Link(ver_a,ver_b,v[2])
+            if not self.is_exist(L):
+                ver_a.list_link.append(L)
+                ver_b.list_link.append(L)
+                self.links.append(L)
+    def print(self):
+        for p in self.verticals:
+            print(p)
+            p.print()
+
+
+class Сycle_basis:
+    def __init__(self, ver_a, ver_b, p):
+        self.ver_a = ver_a
+        self.ver_b = ver_b
+        self.p = p
+
+    def DFS(self,p):
+        p.pai= 1
+        for c in p.verticals:
+
+
+    def obxod(self,g=Graph()):
+       for p in g.verticals:
+          if p.pai == 0:
+              DFS()
+
+
+
+def main():
+    g = Graph()
+    g.load_file('C:/python_graphs/test_1.txt')
+    g.print()
+
+main()
